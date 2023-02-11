b79975cdfe92405a63efd9f08ad7b46cdf01cbe1
Tae-Woong Yoon <twy@korea.ac.kr>
2023-02-11 10:33:46 +0900

    Modify Home.py

--- Home.py
+++ Home.py
@@ -56,7 +56,7 @@ def main():
             2021
           - [Science, Technology, and Gender Diversity](https://youtu.be/xUJ9e_hESG8),
             2020
-          - [Certainties and Uncertainties of Mathematics](https://youtu.be/RMvVxr8czTU),
+          - [Foundation of Mathematics and Kurt Friedrich Gödel](https://youtu.be/RMvVxr8czTU),
             2013
         """
     )
