d28aace80738f00ed7c260b02833475c379a4b1c
Tae-Woong Yoon <twy@korea.ac.kr>
2023-10-21 14:53:24 +0900

    Update 50_🎭_GPT & DALL·E.py

--- pages/50_🎭_GPT & DALL·E.py
+++ pages/50_🎭_GPT & DALL·E.py
@@ -4,11 +4,16 @@ ChatGPT & DALL·E using openai API (by T.-W. Yoon, Mar. 2023)
 
 def redirect_to_chatgpt_dalle_app():
     # Redirects the user to the ChatGPT DALL·E application.
-    import webbrowser
+    import streamlit as st
 
     url = "https://chatgpt-dalle.streamlit.app/"
-    webbrowser.open_new_tab(url)
+    # webbrowser.open_new_tab(url)
+
+    page = f"""
+        <meta http-equiv="refresh" content="0;URL='{url}'" />
+    """
+    st.markdown(page, unsafe_allow_html=True)
 
 
-if __name__ == "__main__":
+if __name__ㄴ== "__main__":
     redirect_to_chatgpt_dalle_app()
