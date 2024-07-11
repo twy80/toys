4b2bdd2a904c6462ec21b27b12a719af9a061b04
Tae-Woong Yoon <twy@korea.ac.kr>
2024-07-11 16:14:48 +0900

    Update 50_🎭_Generative AI.py

--- pages/50_🎭_Generative AI.py
+++ pages/50_🎭_Generative AI.py
@@ -1,28 +1,29 @@
-def openai_generate():
+def generative_ai():
     """
     Calling two apps using OpenAI's APIs
     """
 
     import streamlit as st
 
-    st.write("## 🎭 OpenAI Generators")
+    st.write("## 🎭 Generative AI")
     st.write("")
 
     st.write(
         """
-        ##### Three apps using OpenAI's large language models are presented as follows:
+        ##### Three apps using large language models (LLMs) such as GPT-4o and Gemini-flash
+        are presented as follows:
 
         - [LangChain LLM Agent](https://langchain-llm-agent.streamlit.app/),
-          which implements ChatGPT (with images), RAG (Retrieval Augmented
-          Generation), and DALL·E using OpenAI and LangChain functions.
+          which implements agents using LangChain. GPT models from OpenAI or Gemini models
+          from Google can be used together with tools.
 
         - [OpenAI Assistants](https://assistants.streamlit.app/),
           which enables users to create their own custom
-          chatbots using the Assistants API.
+          chatbots using the Assistants API from OpenAI.
 
         - [Multi-Agent Debate](https://multi-agent-debate.streamlit.app/),
-          which enables two agents to on a given topic. LangChain and OpenAI
-          functions are employed.
+          which enables two agents to debate on a given topic. GPT-4o from OpenAI
+          is employed using LangChain.
 
         In these apps, tools such as searching the internet, ArXiv, Wikipedia,
         or uploaded documents (retrieval) can be used. Python REPL is also
@@ -32,4 +33,4 @@ def openai_generate():
 
 
 if __name__ == "__main__":
-    openai_generate()
+    generative_ai()
